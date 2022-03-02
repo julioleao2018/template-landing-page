46212d8f028b4c02bb618f20e677a98503a8eb4c
Julio <julio.leaojc5@gmail.com>
2022-03-02 16:25:44 -0300

    new section screenshots

--- js/main.js
+++ js/main.js
@@ -43,6 +43,25 @@ $(document).ready(function() {
                 items: 3,
             }
         }
+    });
+
+    // Screenshots carousel
+    $('.screenshots-carousel').owlCarousel({
+        loop: true,
+        margin: 0,
+        autoplay: true,
+        responsiveClass: true,
+        responsive: {
+            0: {
+                items: 1,
+            },
+            600: {
+                items: 2,
+            },
+            1000: {
+                items: 3,
+            }
+        }
     })
 
 });
